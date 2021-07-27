c3bc93d189c3e021ecaca96d51ab454e7211572e
97prince97 <97prince97@gmail.com>
2021-07-27 15:26:25 +0530

    add zomato schema & model

---
+++ server/database/food/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const FoodSchema = new Mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  isVeg: { type: Boolean, required: ture },
+  isContainsEgg: { type: Boolean, required: ture },
+  category: { type: String, required: ture },
+  photos: {
+    type: mongoose.Types.ObjectId,
+    ref: "Images",
+  },
+  price: { type: Number, default: 150, required: true },
+  addOns: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Foods",
+    },
+  ],
+  restaurant: {
+    type: mongoose.Types.ObjectId,
+    ref: "Restaurants",
+    required: true,
+  },
+});
+
+export const FoodModel = mongoose.model("Foods", FoodSchema);
