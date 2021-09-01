30f0e83db07975a7f1185e9c853cb5954dc118a1
97prince97 <97prince97@gmail.com>
2021-09-01 10:20:58 +0530

    add active to PC screen

--- client/src/Components/FoodTab/index.jsx
+++ client/src/Components/FoodTab/index.jsx
@@ -6,6 +6,7 @@ import {
   RiHeartAddLine,
 } from "react-icons/ri";
 import { GiWineGlass } from "react-icons/gi";
+import classnames from "classnames";
 
 const MobileTabs = () => {
   const [allTypes, setAllTypes] = useState([
@@ -13,25 +14,21 @@ const MobileTabs = () => {
       id: "delivery",
       icon: <RiShoppingBag3Line />,
       name: "Delivery",
-      isActive: false,
     },
     {
       id: "dining",
       icon: <RiFootprintLine />,
       name: "Dining Out",
-      isActive: false,
     },
     {
       id: "night",
       icon: <GiWineGlass />,
       name: "Night Life",
-      isActive: false,
     },
     {
       id: "nutri",
       icon: <RiHeartAddLine />,
       name: "Nutrition",
-      isActive: false,
     },
   ]);
 
@@ -66,48 +63,80 @@ const MobileTabs = () => {
 };
 
 const LargeTabs = () => {
+  const [allTypes, setAllTypes] = useState([
+    {
+      id: "delivery",
+      imageDefault:
+        "https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png",
+      imageActive:
+        "https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png",
+      name: "Delivery",
+      activeColor: "yellow",
+    },
+    {
+      id: "dining",
+      imageDefault:
+        "https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png",
+      imageActive:
+        "https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png",
+      name: "Dining Out",
+      activeColor: "blue",
+    },
+    {
+      id: "night",
+      imageDefault:
+        "https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png",
+      imageActive:
+        "https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png",
+      name: "Night Life",
+      activeColor: "purple",
+    },
+    {
+      id: "nutri",
+      imageDefault:
+        "https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png",
+      imageActive:
+        "https://b.zmtcdn.com/data/o2_assets/0f6dcb1aef93fa03ea3f91f37918f3bc1616649503.png",
+      name: "Nutrition",
+      activeColor: "yellow",
+    },
+  ]);
+
+  const { type } = useParams();
+
   return (
     <>
-      <div className="flex items-center gap-2.5">
-        <div className="w-14 h-14 bg-gray-100 p-3 rounded-full">
-          <img
-            src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png"
-            alt="delivery"
-            className="w-full h-full"
-          />
-        </div>
-        <h3 className="text-xl text-gray-500 font-semibold">Delivery</h3>
-      </div>
-      <div className="flex items-center gap-2.5">
-        <div className="w-14 h-14 bg-gray-100 p-3 rounded-full">
-          <img
-            src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png"
-            alt="delivery"
-            className="w-full h-full"
-          />
-        </div>
-        <h3 className="text-xl text-gray-500 font-semibold">Dining Out</h3>
-      </div>
-      <div className="flex items-center gap-2.5">
-        <div className="w-14 h-14 bg-gray-100 p-3 rounded-full">
-          <img
-            src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png"
-            alt="delivery"
-            className="w-full h-full"
-          />
-        </div>
-        <h3 className="text-xl text-gray-500 font-semibold">Night Life</h3>
-      </div>
-      <div className="flex items-center gap-2.5">
-        <div className="w-14 h-14 bg-gray-100 p-3 rounded-full">
-          <img
-            src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png"
-            alt="delivery"
-            className="w-full h-full"
-          />
-        </div>
-        <h3 className="text-xl text-gray-500 font-semibold">Nutrition</h3>
-      </div>
+      {allTypes.map((items) => (
+        <Link to={`/${items.id}`}>
+          <div
+            className={classnames(
+              "flex items-center gap-2.5 pb-2 transition-all duration-500 ease-in-out ",
+              {
+                "border-b-2 border-zomato-400": type === items.id,
+              }
+            )}
+          >
+            <div
+              className={classnames("w-14 h-14 bg-gray-100 p-3 rounded-full", {
+                [`bg-${items.activeColor}-100`]: type === items.id,
+              })}
+            >
+              <img
+                src={type === items.id ? items.imageActive : items.imageDefault}
+                alt="delivery"
+                className="w-full h-full"
+              />
+            </div>
+            <h3
+              className={`text-xl ${
+                type === items.id ? "text-zomato-300" : "text-gray-500"
+              } font-semibold`}
+            >
+              {items.name}
+            </h3>
+          </div>
+        </Link>
+      ))}
     </>
   );
 };
