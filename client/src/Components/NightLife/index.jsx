ea81b053466f4d32368d1eb2a29913dad496eb6c
97prince97 <97prince97@gmail.com>
2021-09-03 14:25:45 +0530

    add night life

---
+++ client/src/Components/NightLife/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import NightLifeCarousel from "./NightLifeCarousel";
+
+const NightLife = () => {
+  return (
+    <>
+      <NightLifeCarousel />
+      <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
+        Nightlife Restaurants in Agra
+      </h1>
+    </>
+  );
+};
+
+export default NightLife;
