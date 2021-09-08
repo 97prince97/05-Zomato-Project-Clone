e8c9debaa1c7ea88fe37c1ce78e0e2fe52305554
97prince97 <97prince97@gmail.com>
2021-09-09 00:55:13 +0530

    add order online food card

---
+++ client/src/Components/restaurant/Order-Online/FoodItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { BsPlus } from "react-icons/bs";
+
+const FoodItem = (props) => {
+  return (
+    <>
+      <div className="flex items-start gap-3 lg:px-5 lg:py-8 py-5">
+        <div className="md:w-4/12 lg:h-32 md:24 w-24 w-3/12">
+          <img
+            src={props.image}
+            alt="food"
+            className="w-full h-full rounded-lg"
+          />
+        </div>
+        <div className="md:w-8/12 w-9/12 flex flex-col">
+          <div className="flex items-center justify-between">
+            <h3 className="text-base font-medium">{props.title}</h3>
+            <button className="md:hidden flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-4 py-1 rounded-lg">
+              Add
+              <BsPlus />
+            </button>
+          </div>
+          <ReactStars count={5} value={props.rating} />
+          <h5 className="font-light">{props.price}</h5>
+          <p className="truncate font-light text-sm md:text-base">
+            {props.description}
+          </p>
+        </div>
+        <div className="hidden md:block md:w-3/12">
+          <button className="flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-4 py-1 rounded-lg">
+            Add
+            <BsPlus />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FoodItem;
--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -1,20 +1,38 @@
 import React from "react";
+import { IoCompassOutline, IoLogoChrome } from "react-icons/io5";
+import { HiOutlineClock } from "react-icons/hi";
 
 // Components
 import FloatMenuBtn from "../../Components/restaurant/Order-Online/FloatMenuBtn";
 import MenuListContainer from "../../Components/restaurant/Order-Online/MenuListContainer";
-
+import FoodItem from "../../Components/restaurant/Order-Online/FoodItem";
 const OrderOnline = () => {
   return (
     <>
-      <div className="w-full">
+      <div className="w-full flex">
         <aside className="hidden md:flex w-1/4 border-r border-gray-300 h-screen overflow-y-scroll flex-col gap-3">
           <MenuListContainer />
           <MenuListContainer />
           <MenuListContainer />
           <MenuListContainer />
         </aside>
-        <div className="w-full md:w-3/4"></div>
+        <div className="w-full md:w-3/4">
+          <h2 className="text-xl font-semibold">Order Online</h2>
+          <h4 className="flex items-center gap-2 text-gray-500 font-light">
+            <IoCompassOutline /> Live track your order | <HiOutlineClock /> 37
+            min
+          </h4>
+          <section>
+            <FoodItem
+              image="https://b.zmtcdn.com/data/dish_photos/e0a/c02eadd59fb1ed566e6837ff022dae0a.jpg?output-format=webp&fit=around|130:130&crop=130:130;*,*"
+              title="
+              Paneer Tikka Butter Masala Pizza"
+              price="₹210"
+              rating="3"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, quae eos minima ad quo in accusamus porro. Debitis alias dolore culpa exercitationem. Distinctio, vero quam. Natus suscipit iste eaque obcaecati."
+            />
+          </section>
+        </div>
       </div>
       <FloatMenuBtn />
     </>
